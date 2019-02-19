7796448fe1431cdfc1b564b02a52d39a9fcc847b
s-karpovich <mdcatalog2@gmail.com>
2019-02-19 22:16:26 +0300

    +

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0622/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task06.task0622;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Числа по возрастанию
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //напишите тут ваш код
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+    }
+
+    public static void findMin(int a, int b, int c, int d, int e) {
+     if (a<b) {swapWithMin (a, b);}
+    }
+
+    public static void swapWithMin(int one, int two) {
+
+    }
+}
+
