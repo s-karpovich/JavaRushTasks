1fb3e660194f56e9ff5d7a351ce95325dcd7befd
s-karpovich <mdcatalog2@gmail.com>
2019-02-21 20:45:32 +0300

    +

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0721/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task07.task0721;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Минимаксы в массивах
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[20];
+        int maximum;
+        int minimum;
+
+        //напишите тут ваш код
+        for (int i = 0; i <arr.length; i++) {
+            arr[i] = Integer.parseInt(reader.readLine());
+        }
+        minimum = arr[0];
+        maximum = arr[0];
+
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i]<minimum) minimum = arr[i];
+            if (arr[i]>maximum) maximum = arr[i];
+        }
+
+        System.out.print(maximum + " " + minimum);
+    }
+}
