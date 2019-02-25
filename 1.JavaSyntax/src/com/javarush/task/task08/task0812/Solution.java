1edd950fe435a5c4894095082976719ecfc90125
s-karpovich <mdcatalog2@gmail.com>
2019-02-25 18:04:11 +0300

    +

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 Cамая длинная последовательность
 */
 public class Solution {
+
     public static void main(String[] args) throws IOException {
         //напишите тут ваш код
         int count = 1;
