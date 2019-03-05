a04745d2d142aba54c82423dbb0fbf088b1dae9b
s-karpovich <mdcatalog2@gmail.com>
2019-03-05 20:51:57 +0300

    +

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1315/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task13.task1315;
+
+/* 
+Том, Джерри и Спайк
+*/
+
+import com.sun.media.sound.ModelChannelMixer;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //может двигаться
+    public interface Movable {
+        void move();
+    }
+
+    //может быть съеден
+    public interface Edible {
+        void beEaten();
+    }
+
+    //может кого-нибудь съесть
+    public interface Eat {
+        void eat();
+    }
+
+    public class Cat implements Eat, Movable, Edible {
+        @Override
+        public void move() {
+        }
+
+        @Override
+        public void beEaten() {
+        }
+
+        @Override
+        public void eat() {
+        }
+    }
+
+    public class Dog implements Eat, Movable {
+        @Override
+        public void move() {
+        }
+
+        @Override
+        public void eat() {
+        }
+    }
+
+    public class Mouse implements Movable, Edible {
+        @Override
+        public void move() {
+
+        }
+        @Override
+        public void beEaten() {
+
+        }
+    }
+}
