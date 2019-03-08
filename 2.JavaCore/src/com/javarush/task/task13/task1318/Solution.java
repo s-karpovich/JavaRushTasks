de62841eb64daf140eb66755dfd2b6b47fe18296
s-karpovich <mdcatalog2@gmail.com>
2019-03-08 21:47:56 +0300

    +

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -1,17 +1,27 @@
 package com.javarush.task.task13.task1318;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Scanner;
-import java.io.FileInputStream;
 
 /* 
 Чтение файла
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // напишите тут ваш код
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String name = bf.readLine();
 
+
+        InputStream inputStream = new FileInputStream(name);
+        Solution myObject = new Solution();
+        while (inputStream.available() > 0) {
+            int data = inputStream.read();
+            System.out.print(data);
+        }
+
+        inputStream.close();
+        bf.close();
     }
 }
