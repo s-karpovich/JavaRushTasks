ca8a9076ec7b86cda69d290e7205275761139495
s-karpovich <mdcatalog2@gmail.com>
2019-03-24 20:18:51 +0300

    +

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -6,15 +6,26 @@ package com.javarush.task.task18.task1821;
 
 import java.io.*;
 import java.lang.reflect.Array;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        InputStream input= new FileInputStream("e:/baton.txt");
-        List<>
+        InputStream input= new FileInputStream(args[0]);
+        List<Character> list = new ArrayList<>();
+        Set<Character> treeSet = new TreeSet<>();
+
+
+
         StringBuilder sb = new StringBuilder();
         while(input.available()>0) {
-          list.add()
+          list.add((char) input.read());
         }
-        System.out.println(sb);
+        input.close();
+
+        treeSet.addAll(list);
+
+        for (Character character : treeSet) {
+            System.out.println(character+" "+Collections.frequency(list, character));
+        }
     }
 }
